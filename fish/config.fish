23282a5687fcb54ab9d6103323ad7367ea5a1a8b
GodLovesJonny <zijiewang9928@gmail.com>
2019-12-21 12:44:15 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,8 @@ function time --description="Base time function"
 end
 
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
+
+alias ra='ranger'
+alias cl='clear'
+alias ipy='ipython'
+alias jn='jupyter notebook'
